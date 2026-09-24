bda5f9fdd4fcbc5c9ee5a4d0d92fd5672215b329
JoseCS96 <140642015+JoseCS96@users.noreply.github.com>
2026-09-24 17:19:47 -0500

    Add lot generation API calls

--- src/modules/lotes/api.ts
+++ src/modules/lotes/api.ts
@@ -1,15 +1,20 @@
 import axios from "axios";
-import type { LoteListado, LotesFiltros } from "./types";
+import type { CatalogosLote, GenerarLoteRequest, GenerarLoteResponse, LoteListado, LotesFiltros } from "./types";
 
-const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL ?? "",
-});
+const api = axios.create({ baseURL: import.meta.env.VITE_API_URL ?? "" });
 
 export async function listarLotes(filtros: LotesFiltros = {}) {
-  const params = Object.fromEntries(
-    Object.entries(filtros).filter(([, value]) => value !== undefined && value !== "")
-  );
-
+  const params = Object.fromEntries(Object.entries(filtros).filter(([, value]) => value !== undefined && value !== ""));
   const { data } = await api.get<LoteListado[]>("/api/lotes", { params });
+  return data;
+}
+
+export async function obtenerCatalogosLote() {
+  const { data } = await api.get<CatalogosLote>("/api/lotes/catalogos");
+  return data;
+}
+
+export async function generarLote(request: GenerarLoteRequest) {
+  const { data } = await api.post<GenerarLoteResponse>("/api/lotes/generar", request);
   return data;
 }
